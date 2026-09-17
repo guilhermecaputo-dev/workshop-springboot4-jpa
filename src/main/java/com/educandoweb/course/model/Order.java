a13eee7ec66c3ad7bcedd4af276ba0ddd42433c9
Guilherme <gmanenticaputo@gmail.com>
2026-09-17 12:11:34 -0300

    OrderStatus Enum

--- src/main/java/com/educandoweb/course/model/Order.java
+++ src/main/java/com/educandoweb/course/model/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.model;
 
+import com.educandoweb.course.model.enuns.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -19,15 +20,29 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valorOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
